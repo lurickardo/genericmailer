16330b7189bc5da9fa72111003ee56e3313e2063
Luiz Ricardo Soares dos Santos <luizr726@gmail.com>
2021-06-30 20:47:49 -0300

    Finalizando implementaçao de envio de e-mail para contato

---
+++ src/routes.ts
@@ -0,0 +1,12 @@
+import { Router, Request, Response } from 'express'
+import contactUsRoutes from './routes/contactUsRoutes'
+
+const routes = Router()
+
+routes.use('/api/', contactUsRoutes)
+
+routes.use(function (request: Request, response: Response) {
+    response.sendStatus(404);
+});
+
+export default routes
